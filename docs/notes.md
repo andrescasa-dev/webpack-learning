a68ae91e23c986d053d86bc1bc057a85348eba39
andrescasa-dev <andrescasa.dev@gmail.com>
2022-05-10 17:41:13 -0500

    babel was config as well as html and css files

---
+++ docs/notes.md
@@ -0,0 +1,4 @@
+## Path
+1. Install webpack
+2. config: output, input and resolve:extensions
+3. install babel
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve('dist'),
+    path: path.resolve(__dirname,'dist'),
     filename: 'bundle.js'
   },
   resolve:{
-    extension: '.js'
-  }
+    extensions: ['.js']
+  },
+  module:{
+    rules:[
+      {
+        test: /\.m?js$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: './public/index.html',
+      }
+    ),
+    new MiniCssExtractPlugin(),
+  ],
 }
